67a5be71aceb5da370dc45ffe8aa72cd3df3367b
Kuprin Aleksandr <freeman8901@yandex.ru>
2025-12-12 21:37:55 +0500

    Главная страница перед тестированием

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from datetime import datetime
 
@@ -7,8 +8,23 @@ import requests
 from dotenv import load_dotenv
 
 
+path_log_directory = os.path.abspath("../logs/utils.log")
+logger = logging.getLogger(__name__) if __name__ != "__main__" else logging.getLogger("src.utils")
+file_handler = logging.FileHandler(path_log_directory, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def user_greeting(str_time: str) -> str:
+    """
+    Функция формирования приветствия по времени суток
+    :param str_time: Текущее время в формате строки
+    :return: Сформированное приветствие
+    """
     int_hour = int(str_time[:2])
+    logger.info('Формирование приветствия')
     if int_hour > 5 and int_hour < 12:
         return 'Доброе утро'
     elif int_hour > 11 and int_hour < 18:
@@ -19,52 +35,74 @@ def user_greeting(str_time: str) -> str:
         return 'Доброй ночи'
 
 
-def read_operations_from_excel(path: str) -> list:
+def read_operations_from_excel(path: str) -> list | None:
+    """
+    Функция чтения операций пользователя из xlsx-файла
+    :param path: Путь к xlsx-файлу
+    :return: Операции пользователя в формате списка
+    """
     path_excel_file_operation = os.path.abspath(path)
     try:
         excel_data = pd.read_excel(path_excel_file_operation)
-        # logger.info("Данные с xlsx-файла прочитаны")
+        logger.info("Данные с xlsx-файла прочитаны")
         return excel_data.to_dict("records")
     except FileNotFoundError:
-        # logger.critical("XLSX-файл не найден")
+        logger.critical("XLSX-файл не найден")
         return []
 
 
-def read_user_settings_from_json(path: str) -> list:
+def read_user_settings_from_json(path: str) -> dict | None:
+    """
+    Функция чтения настроек пользователя
+    :param path: Путь к файлу настроек пользователя
+    :return: Настройки пользователя в формате словаря
+    """
     path_json_file = os.path.abspath(path)
     try:
         with open(path_json_file, "r", encoding="utf-8") as json_file:
             data = json.load(json_file)
+            logger.info("Данные с json-файла прочитаны")
     except FileNotFoundError:
-        # logger.critical("Файл json не найден")
-        # logger.critical(file_name)
-        # logger.critical(os.path.dirname(__file__))
-        # logger.critical(path_json_file)
+        logger.critical("Файл json не найден")
+        logger.critical(os.path.dirname(__file__))
+        logger.critical(path_json_file)
         return []
 
     if len(data) == 0 or type(data) is not dict:
-        # logger.error("Нет данных или не верный формат данных")
+        logger.error("Нет данных или не верный формат данных")
         return []
     else:
-        # logger.info("Получены данные по транзакциям из json-файла")
+        logger.info("Получены данные по транзакциям из json-файла")
         return data
 
 
 def selection_of_operations_for_analysis(data: list, date_now: str) -> list:
+    """
+    Функция отбора операций для анализа и группировки
+    :param data: Операции пользователя в формате списка
+    :param date_now: Текущая или необходимая дата в формате строки
+    :return: Отобранные операции с формате списка
+    """
     selected_operations = []
     start_date = datetime.strptime('01' + date_now[2:], '%d.%m.%Y')
     end_date = datetime.strptime(date_now + ' 23:59:59', '%d.%m.%Y %H:%M:%S')
-
+    logger.info('Отбор операций начат')
     for operation in data:
         select_date = datetime.strptime(operation['Дата операции'], '%d.%m.%Y %H:%M:%S')
         if (start_date <= select_date <= end_date) and operation['Статус'] == 'OK':
             selected_operations.append(operation)
-
+    logger.info('Отбор операций завершен')
     return selected_operations
 
 
 def processing_operations_main(data: list) -> list:
+    """
+    Функция группировки операций по номерам карт
+    :param data: Отобранные операции пользователя для группировки
+    :return: Сгруппированные операции пользоватля в формате списка
+    """
     list_data = []
+    logger.info('Группировка операций начата')
     for operation in data:
         if type(operation['Номер карты']) == float:
             operation['Номер карты'] = '_NaN_'
@@ -88,16 +126,42 @@ def processing_operations_main(data: list) -> list:
                 temp_dict['total_spent'] = operation['Сумма операции с округлением']
                 temp_dict['cashback'] = operation['Бонусы (включая кэшбэк)']
                 list_data.append(temp_dict)
+    logger.info('Группировка операций завершена')
     return list_data
 
 
-def processing_operations_main_top_five(data: list) -> list:
+def processing_operations_main_top_five(data: list) -> list | None:
+    """
+    Функция отбора ТОП-5 операций пользователя за выбранный период
+    :param data: Отобранные для анализа операции пользователя в ормате списка
+    :return: Отобранные ТОП-5 операций пользователя в формате списка
+    """
+    logger.info('Отбор топ 5 операций начат')
+    if len(data) == 0 or type(data) is not list:
+        return []
     df = pd.DataFrame(data)
     sort_list = df.sort_values('Сумма операции с округлением', ascending=False)[:5]
-    return sort_list.to_dict("records")
+    list_top_five_transactions = sort_list.to_dict("records")
+    logger.info('Отбор топ 5 операций завершен')
+    temp_list = []
+    for item in list_top_five_transactions:
+        temp_dict = {}
+        temp_dict['date'] = item['Дата платежа']
+        temp_dict['amount'] = item['Сумма операции с округлением']
+        temp_dict['category'] = item['Категория']
+        temp_dict['description'] = item['Описание']
+        temp_list.append(temp_dict)
+    logger.info('Преобразование данных завершено')
+    return temp_list
 
 
-def exchange_rates(currencies: list = ["USD", "EUR"]) -> float | None:
+def exchange_rates(currencies: list = ["USD", "EUR"]) -> list | None:
+    """
+    Функция запроса котировок валют на текущее время
+    :param currencies: Список валют, по которым отправляется запрос на котировки в формате списка
+    :return: Котировки валют в формате списка
+    """
+    logger.info('Запрос котировок валют начат')
     load_dotenv()
     api_key = os.getenv("API_KEY_EXCHANGE_RATES")
     base_ = "RUB"
@@ -105,30 +169,54 @@ def exchange_rates(currencies: list = ["USD", "EUR"]) -> float | None:
     url = (f"https://api.apilayer.com/exchangerates_data/latest?symbols={symbols_}&base={base_}")
     headers = {"apikey": api_key}
     response = requests.get(url, headers=headers)
-
+    logger.info('Получена реакция на запрос')
     status_code = response.status_code
     if status_code == 200:
         result = response.json()
+        logger.info('Ответ на запрос сформирован')
     else:
-        return {}
+        logger.error(f'Ошибка запроса - {status_code}. Ответ на запрос не сформирован. Возращен пустой список')
+        return []
 
     for keys, values in result['rates'].items():
         result['rates'][keys] = round(1 / values, 2)
+    logger.info('Котировки приведены в соответствие')
+    temp_list = []
+    for keys, values in result['rates'].items():
+        temp_dict = {}
+        temp_dict['currency'] = keys
+        temp_dict['rate'] = values
+        temp_list.append(temp_dict)
+    logger.info('Преобразование данных завершено')
+    return temp_list
 
-    return result
 
-
-def stock_prices(stocks: list = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]) -> float | None:
+def stock_prices(stocks: list = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]) -> list | None:
+    """
+    Функция запроса котировок акций
+    :param stocks: Список акций, по которым делается запрос на котировки в формате списка
+    :return: Котировки акций в формате списка
+    """
+    logger.info('Запрос котировок акций начат')
     load_dotenv()
     api_key = os.getenv("API_KEY_STOCK_PRICES")
     symbol_ = ','.join(stocks)
     url = (f"https://api.twelvedata.com/price?symbol={symbol_}&apikey={api_key}")
     response = requests.get(url)
-
+    logger.info('Получена реакция на запрос')
     status_code = response.status_code
     if status_code == 200:
         result = response.json()
+        logger.info('Ответ на запрос сформирован')
     else:
-        return {}
+        logger.error(f'Ошибка запроса - {status_code}. Ответ на запрос не сформирован. Возращен пустой список')
+        return []
 
-    return result
+    temp_list = []
+    for keys, values in result.items():
+        temp_dict = {}
+        temp_dict['stock'] = keys
+        temp_dict['price'] = values['price']
+        temp_list.append(temp_dict)
+    logger.info('Преобразование данных завершено')
+    return temp_list
--- src/views.py
+++ src/views.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import os
 from datetime import datetime
 
 from src.utils import user_greeting, read_operations_from_excel, selection_of_operations_for_analysis, \
@@ -6,52 +8,60 @@ from src.utils import user_greeting, read_operations_from_excel, selection_of_op
     read_user_settings_from_json
 
 
+path_log_directory = os.path.abspath("../logs/views.log")
+logger = logging.getLogger(__name__) if __name__ != "__main__" else logging.getLogger("src.views")
+file_handler = logging.FileHandler(path_log_directory, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def home_page(datetime_now: datetime) -> list[dict]:
+    """
+    Функция главной страницы
+    :param datetime_now: Необходимая дата и время в формате datetime
+    :return: Сформированные данные в формате json-строки
+    """
     time_now = datetime.strftime(datetime_now, "%H:%M:%S")
     date_now = datetime.strftime(datetime_now, "%d.%m.%Y")
+    user_settings_read = 1
     time_now = '17:34:45' #  На время написания функционала
-    date_now = '16.11.2021' #  На время написания функционала
-
+    date_now = '02.11.2021' #  На время написания функционала
+    logger.info('Установлены дата и время')
     user_settings = read_user_settings_from_json('../user_settings.json')
-
+    if user_settings == list():
+        user_settings_read = 0
+    logger.info('Прочитан файл настроек пользователя')
     greeting = user_greeting(time_now)
+    logger.info('Сформировано приветствие')
     list_user_operations = read_operations_from_excel('../data/operations.xlsx')
+    logger.info('Прочитаны операции пользователя')
     list_user_operations_per_month = selection_of_operations_for_analysis(list_user_operations, date_now)
+    logger.info('Отбор операций за месяц произведен')
     list_sum_operations = processing_operations_main(list_user_operations_per_month)
+    logger.info('Операции по картам сгруппированы')
     list_top_five_transactions = processing_operations_main_top_five(list_user_operations_per_month)
-    exchange_rates_now = exchange_rates(user_settings['user_currencies'])
-    stock_prices_now = stock_prices(user_settings['user_stocks'])
+    logger.info('Отбор операций топ 5 произведен')
+    if user_settings_read == 1:
+        exchange_rates_now = exchange_rates(user_settings['user_currencies'])
+        logger.info('Котеровки валют прочитаны')
+        stock_prices_now = stock_prices(user_settings['user_stocks'])
+        logger.info('Котировки акций прочитаны')
+    else:
+        exchange_rates_now = exchange_rates()
+        logger.info('Котеровки валют прочитаны')
+        stock_prices_now = stock_prices()
+        logger.info('Котировки акций прочитаны')
     json_answer = {}
     json_answer['greeting'] = greeting
     json_answer['cards'] = list_sum_operations
-
-    temp_list = []
-    for item in list_top_five_transactions:
-        temp_dict = {}
-        temp_dict['date'] = item['Дата платежа']
-        temp_dict['amount'] = item['Сумма операции с округлением']
-        temp_dict['category'] = item['Категория']
-        temp_dict['description'] = item['Описание']
-        temp_list.append(temp_dict)
-    json_answer['top_transactions'] = temp_list
-
-    temp_list = []
-    for keys, values in exchange_rates_now['rates'].items():
-        temp_dict = {}
-        temp_dict['currency'] = keys
-        temp_dict['rate'] = values
-        temp_list.append(temp_dict)
-    json_answer['currency_rates'] = temp_list
-
-    temp_list = []
-    for keys, values in stock_prices_now.items():
-        temp_dict = {}
-        temp_dict['stock'] = keys
-        temp_dict['price'] = values['price']
-        temp_list.append(temp_dict)
-    json_answer['stock_prices'] = temp_list
-
+    json_answer['top_transactions'] = list_top_five_transactions
+    json_answer['currency_rates'] = exchange_rates_now
+    json_answer['stock_prices'] = stock_prices_now
+    logger.info('Сформирован json-ответ для главной страницы')
     return json.dumps(json_answer, ensure_ascii=False)
 
 if __name__ == '__main__':
+
     print(home_page(datetime.now()))
